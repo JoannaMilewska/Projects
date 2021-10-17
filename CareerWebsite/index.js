d33df157649a99ce01f3c47f11f44b020c73267a
JoannaMilewska <44784410+JoannaMilewska@users.noreply.github.com>
2021-10-17 20:58:32 +0200

    Done project.

---
+++ CareerWebsite/index.js
@@ -0,0 +1,74 @@
+//let input = prompt("hey, say sth!")
+//while (true){
+ //   input= prompt(input);
+//    if( input.toLowerCase()==="stop copying me")break;
+//}
+//console.log("you win")
+
+/*let maximum = parseInt(prompt(' write your maximum number'));
+while (!maximum) {
+     maximum = parseInt(prompt(' write a valid number number'));
+}
+const targetNum = Math.floor(Math.random()*maximum)+1;
+let guess = parseInt(prompt(' enter your first guess'));
+let attempts = 1;
+
+while (guess !== targetNum) {
+    attempts++;
+    if (guess > targetNum) {
+        guess = parseInt(prompt("your number is too high. Enter a new guess:"));
+    }else {
+        guess = parseInt(prompt("your number is too  low.Enter a new guess:"));
+
+    }
+}
+console.log (`You got it in ${attempts}` guesses) */
+/*const toDoList=["dupa", "dupa"]  ;
+let decision = prompt("what would you like to do in the app?")
+while (decision !=='quit' && decision!=='q'){
+    if (decision==='list'){
+        console.log("*****")
+        for (let i=0; i<toDoList.length;i++){
+            console.log(`${i}: ${toDoList[i]}`); }
+        console.log("****")
+    }
+    else if(decision==='new'){
+        const newTodo=prompt("so whats new to do?");
+        toDoList.push(newTodo);
+        console.log(`${newTodo}added to the list`)
+    }
+    else if (input==='delete'){
+        const index =prompt('which index would you like to delete?')
+        console.log(index);
+
+    }*/
+        
+
+   /* }
+    decision = prompt("what would you like to do in the app?")
+}
+console.log('OK BYE')
+
+   // let newThing = prompt('so what are you going to do?')
+    //var toDoListUpdate = toDoList.push(newThing);
+   // console.log('added to toDoList')
+//}
+//while ( decision="quit")[
+  //  console.log('ok see you next time')
+//]
+*/
+
+/*let n = 21;
+let lista =[1,1];
+
+if (n < 3) {
+    console.log('podaj większa liczbe')
+}
+else {
+    for (let i=2; i<=n; i++)
+    lista.push(lista[i-2]+lista[i-1])
+
+console.log(lista)
+
+}*/
+
